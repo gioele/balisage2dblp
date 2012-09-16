03ead9315d7325cff62519b8e6701567aef7f556
Gioele Barabucci <gioele@svario.it>
2012-09-16 14:55:24 +0200

    Download as many TOCs as possible

--- fetch-tocs.sh
+++ fetch-tocs.sh
@@ -2,7 +2,9 @@
 
 mkdir -p "balisage"
 
-for num in `seq 1 9` ; do
+for num in `seq 1 20` ; do
 	toc="http://www.balisage.net/Proceedings/vol${num}/contents.html"
 	wget -c ${toc} -O "balisage/balisage-vol${num}.html"
+
+	[ $? -ne 0 ] && echo "Balisage volume ${num} not found, stopping here" && break
 done
